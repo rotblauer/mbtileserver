e5cdd2856ab1388a3a6ce46ef5de869f656e22a1
ia <b5c6@protonmail.com>
2019-09-22 12:51:19 -0400

    add file event watching and reloading for basedir

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/crypto/acme/autocert"
-
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/labstack/echo"
 
@@ -162,6 +162,61 @@ func serve() {
 	e.GET("/*", h)
 	a := echo.WrapHandler(svcSet.ArcGISHandler(ef))
 	e.GET("/arcgis/rest/services/*", a)
+
+	// Watch tile path for changes and update tileset handler (db conn's too) accordingly.
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+	go func() {
+		log.Println("watching events", tilePath)
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					log.Warnln("watcher event not ok, quitting watcher")
+					return
+				}
+				log.Println("event:", event)
+
+				if !strings.Contains(filepath.Ext(event.Name), "mbtiles") {
+					continue
+				}
+
+				if (event.Op&fsnotify.Write == fsnotify.Write) || (event.Op&fsnotify.Create == fsnotify.Create) {
+					log.Println("modified file:", event.Name)
+
+					log.Println("adding", event.Name)
+					if err := svcSet.AddOrUpdateDBOnPath(tilePath, event.Name); err != nil {
+						log.Println("error:", err)
+					}
+				}
+
+				if (event.Op&fsnotify.Remove == fsnotify.Remove) || (event.Op&fsnotify.Rename == fsnotify.Rename ) {
+					log.Println("created file:", event.Name)
+
+					log.Println("removing", event.Name)
+					if err := svcSet.RemoveDBOnPath(tilePath, event.Name); err != nil {
+						log.Println("error:", err)
+					}
+				}
+
+				h = echo.WrapHandler(svcSet.Handler(ef, true))
+				e.GET("/*", h)
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+	err = watcher.Add(tilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Start the server
 	fmt.Println("\n--------------------------------------")
